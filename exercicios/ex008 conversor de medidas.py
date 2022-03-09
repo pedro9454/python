6695f61307c71bdde70e73c0838824a024b86511
Pedro Henrique Dos Santos Camara <72395676+pedro9454@users.noreply.github.com>
2022-03-09 17:58:38 -0300

    Novos exercícios

---
+++ exercicios/ex008 conversor de medidas.py
@@ -0,0 +1,13 @@
+medida = float(input('Uma distância em metros: '))
+cm = medida * 100
+mm = medida * 1000
+km = medida / 1000
+hm = medida / 100
+de = medida / 10
+dc = medida * 10
+print('A medida de {}m corresponde a: \n{}cm  \n{}mm \n{}Km \n{}hm \n{}de \n{}dc'.format(medida, cm, mm, km, hm, de, dc,))
+
+
+
+
+
