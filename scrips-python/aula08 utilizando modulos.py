1b5d7e4c33e4dd3b0989e208f537e46c79cfc884
Pedro Henrique Dos Santos Camara <72395676+pedro9454@users.noreply.github.com>
2022-03-15 10:44:23 -0300

    Aula 8

---
+++ scrips-python/aula08 utilizando modulos.py
@@ -0,0 +1,10 @@
+from math import sqrt, floor
+num = int(input('Dgite um número: '))
+raiz = sqrt(num)  # sqrt = é raiz quadrada
+print('A raiz de {} é igual a {}'.format(num, floor(raiz)))  # ceil = arredonda para cima 
+
+# import math = vai importar a biblioteca inteira
+# from math inport = vai importar so uma especifica
+# sqrt = é raiz quadrada
+# ceil = arredonda para cima
+# floor = arredonda para baixo
---
+++ scrips-python/aula08b.py
@@ -0,0 +1,7 @@
+import random
+num = random.random()
+print(num)
+
+# random.random() = gera um número inteiro entre 0 e 1
+
+# random.randint(1, 10) = gera um número inteiro de 1 até 10
