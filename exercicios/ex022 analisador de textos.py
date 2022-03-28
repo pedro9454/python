4ea22c36e00876c3f52432778d05a5ed808d5064
Pedro Henrique Dos Santos Camara <72395676+pedro9454@users.noreply.github.com>
2022-03-28 17:02:15 -0300

    Fiz o ex 22

---
+++ exercicios/ex022 analisador de textos.py
@@ -0,0 +1,16 @@
+nome = str(input('Digite seu noem completo: ')).strip()  # strip = elimina os espaços desnecessarios
+print('Seu nome em maiúsculo é {}'.format(nome.upper()))
+print('Seu nome em minúsculas é {}'.format(nome.lower()))
+print('Seu noem tem ao todo {} letras'.format(len(nome) - nome.count(' ')))
+separa = nome.split()
+print('Seu primeiro nome é {} e ele tem {} letras'.format(separa[0], len(separa[0])))
+# print('O seu primeiro nome tem {} letras'.format(nome.find(' ')))
+
+# strip = elimina os espaços desnecessarios
+# upper = muda as letras para maiúsculas
+# lower = muda as letras para minúsculas
+# lean = conta quantos caracteres tem na string
+# count = conta quantas coisas que foram colocadas em parenteses
+
+
+
---
+++ scrips-python/aula09 manipulando textos.py
@@ -0,0 +1,7 @@
+frase = 'Curso em video Python'
+print(frase[5::2])
+
+print('''jfafj aj fjaljsfkjasj fkasjfkaj sfjsajfljslkjjf
+a fsjdfjs flasjfkldjfdjsfjasjfjaskjfsjfk sdkf 
+sjfsd fskjfs asfjlkds fjsf sdjflksjfslfs
+s fslfjklsdjfsjfjlsjfklsjkfksldfk dsjfsdf''')
