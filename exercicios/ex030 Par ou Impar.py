0bfeb37bafcbfe961243e7787042aa22c5f9fc8a
Pedro Henrique Dos Santos Camara <72395676+pedro9454@users.noreply.github.com>
2022-04-12 14:01:37 -0300

    Fiz o ex 30 p

---
+++ exercicios/ex030 Par ou Impar.py
@@ -0,0 +1,12 @@
+# Crie um progma que leia um número inteiro e mostre na tela se ele é PAR ou ÍMPAR.
+numero = int(input('Digite um número: '))
+resultado = numero % 2
+if resultado == 0:
+    print('O número é par!')
+else:
+    print('O númeor é impar!')
+
+# O resto da divisão de qualquer número par por dois é zero
+# O resto da divisão de qualquer número ímpar por dois é um
+
+
