4d8b8410fc32f02882f6ebffdad1b41e15ff2a30
Pedro Henrique Dos Santos Camara <72395676+pedro9454@users.noreply.github.com>
2022-04-13 16:16:00 -0300

    Fiz o ex 31

---
+++ exercicios/ex031b.py
@@ -0,0 +1,7 @@
+# Desenvolva um progama que pergunte a ditância de uma viagem em km. Calcule o preço da passagem, cobrando R$0,50 por Km para viagens de até 200Km e R$0,45 para viagens mais longas.
+
+distancia = float(input('Qual é a distanci da sua viagem? '))
+print('você está prestes a começar uma viagem de {}Km.'.format(distancia))
+preço = distancia * 0.50 if distancia <= 200 else preço = distancia * 0.40
+print('E o preço da sua passagem será de R${:.2f}'.format(preço))
+
