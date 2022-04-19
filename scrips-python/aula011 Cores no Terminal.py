1520bcbfae93e2f3ecd31405557280050800ba65
Pedro Henrique Dos Santos Camara <72395676+pedro9454@users.noreply.github.com>
2022-04-19 16:39:10 -0300

    Fiz o ex de cores

---
+++ scrips-python/aula011 Cores no Terminal.py
@@ -0,0 +1,6 @@
+print('\033[4;30;43mOlá, Mundo!\033[m')
+a = 3
+b = 5
+print('Os valores são \033[36m{}\033[m e \033[33m{}\033[m'.format(a, b))
+nome = 'Pedro Henrique'
+print('Olá! Muito prazer em te conhecer, {}{}{}'.format('\033[4;31m', nome, '\033[m'))  # colocando a cor no format
