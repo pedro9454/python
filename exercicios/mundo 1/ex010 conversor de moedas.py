e883f7019e0f0b08749437e2f4d3d64f2b039acf
Pedro Henrique <phenrique.camara@hotmail.com>
2022-06-11 10:36:19 -0300

    alterações

--- exercicios/mundo 1/ex010 conversor de moedas.py
+++ exercicios/mundo 1/ex010 conversor de moedas.py
@@ -1,5 +1,5 @@
 real = float(input('Quanto dinheiro você tem na carteira? R$'))
-dolar = real / 5.01
+dolar = real / 4.99
 iene = real / 0.043
 euro = real / 5.51
 print('Com tantos R${:.2f} você pode comprar: \nUS${:.2f} \n¥{:.2f} \n€{:.2}'.format(real, dolar, iene, euro))
